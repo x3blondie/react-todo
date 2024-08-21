6ac159d07000478aebc45a54c391e9bca77c9268
Jennifer <jnmurrin@gmail.com>
2024-08-21 12:15:16 -0500

    add completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,10 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = () => {
+export const Todo = ({task, toggleComplete}) => {
   return (
     <div className='Todo'>
-        <p>Go to school</p>
+        <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ''}`}>{task.task}</p>
         <div>
             <FontAwesomeIcon icon={faPenToSquare} />
             <FontAwesomeIcon icon={faTrash} />
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
 import { v4 as uuidv4 } from 'uuid'
+import { Todo } from './Todo'
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -9,9 +10,17 @@ export const TodoWrapper = () => {
     const addTodo = todo => {
         setTodos([...todos, {id: uuidv4(), task: todo, complete: false, isEditing: false}])
     }
+
+    const toggleComplete = id => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo))
+    }
   return (
     <div className='TodoWrapper'>
+        <h1>Get Things Done!</h1>
         <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => (
+            <Todo task={todo} key={index} toggleComplete={toggleComplete}/>
+        ))}
     </div>
   )
 }
