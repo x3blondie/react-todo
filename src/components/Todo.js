2fbfc5a467f90950e29ea8e876d77f36b87bb0ca
Jennifer <jnmurrin@gmail.com>
2024-08-21 11:37:33 -0500

    add todos

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+export const Todo = () => {
+  return (
+    <div className='Todo'>
+        <p>Go to school</p>
+        <div>
+            <FontAwesomeIcon icon={faPenToSquare} />
+            <FontAwesomeIcon icon={faTrash} />
+        </div>
+    </div>
+  )
+}
